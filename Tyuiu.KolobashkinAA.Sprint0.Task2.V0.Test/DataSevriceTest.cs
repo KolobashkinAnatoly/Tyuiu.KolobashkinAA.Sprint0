005fde5b95941ae0e3b56e96a53778617b0913f3
Анатолий Колобашкин <Tyuiu.KolobashkinAA@outlook.com>
2026-09-14 01:41:58 +0500

    Task2

---
+++ Tyuiu.KolobashkinAA.Sprint0.Task2.V0.Test/DataSevriceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KolobashkinAA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KolobashkinAA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataSevriceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+
+        {
+            var name = "Игорь";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Игорь", res);
+
+        }
+    }
+}
