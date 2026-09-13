ca7a3569dc86daa9b0be9c170f82acebca00eb1d
Анатолий Колобашкин <Tyuiu.KolobashkinAA@outlook.com>
2026-09-14 00:34:48 +0500

    Task2

---
+++ Tyuiu.KolobashkinAA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KolobashkinAA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KolobashkinAA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки 
+            var name = "Игорь";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет, Игорь", res);
+        }
+    }
+}
